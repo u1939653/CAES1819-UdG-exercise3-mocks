acb7b17411bd251ed6e3f84e5cae84d33436048b
Jaume Oliveras Hernández <35967311+u1939653@users.noreply.github.com>
2018-11-05 17:54:23 +0100

    Implementat testTransfer
    Testejat el metode "transfer" de la classe Account

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -12,9 +12,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAccountService {
 
-
   @Test
-  void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+  void testTransfer(@Tested AccountService as, @Mocked AccountManager am)
+  {
+    Account l_acc1 = new Account("acc1",10);
+    Account l_acc2 = new Account("acc2",5);
+
+    new Expectations()
+    {{
+      am.findAccount("acc1"); result = l_acc1;
+      am.findAccount("acc2"); result = l_acc2;
+    }};
+
+    as.setAccountManager(am);
+    as.transfer("acc2","acc1",5);
+
+    assertEquals(l_acc1.getBalance(),15);
+    assertEquals(l_acc2.getBalance(),0);
+
+    new Verifications()
+    {{
+      am.updateAccount(l_acc1); times = 1;
+      am.updateAccount(l_acc2); times = 1;
+    }};
+
   }
 }
